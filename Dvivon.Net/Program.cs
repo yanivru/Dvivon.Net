dd02f2ac0814a9bb6f073c717adc238870c89f5c
Yaniv ru <Yaniv.ru@gmail.com>
2022-10-05 23:16:39 +0300

    Saving types to csv file

--- Dvivon.Net/Program.cs
+++ Dvivon.Net/Program.cs
@@ -1,19 +1,54 @@
-﻿using Microsoft.Diagnostics.Runtime;
+﻿using CsvHelper;
+using Microsoft.Diagnostics.NETCore.Client;
+using Microsoft.Diagnostics.Runtime;
 using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
 
 Console.WriteLine("Starting memory dump essence extraction...");
 
 var processingStopwatch = Stopwatch.StartNew();
 
 var commandLineArgs = Environment.GetCommandLineArgs();
-if(commandLineArgs.Length == 1)
+if (commandLineArgs.Length == 1)
 {
     Console.WriteLine("Error: Dump path not specified.");
+    return;
 }
 
 using (var dataTarget = DataTarget.LoadDump(commandLineArgs[1]))
 {
     var clrRuntime = dataTarget.ClrVersions[0].CreateRuntime();
     IEnumerable<ClrObject> objects = clrRuntime.Heap.EnumerateObjects();
-    objects.Select(x => x.Ty)
+
+    var objectsPerType = objects.ToLookup(x => x.Type?.Name ?? "", x => new DumpObject(x.Type?.Name ?? "", x.Size, GetRefencedTypesNames(x).ToArray()));
+
+    var typesStatistics = objectsPerType.Select(x => new DumpType(x.Key, x.Count(), x.Sum(y => (decimal)y.Size)));
+
+    using (var writer = new StreamWriter("Types.csv"))
+    {
+        using (CsvWriter csvWriter = new(writer, CultureInfo.InvariantCulture))
+        {
+            csvWriter.WriteRecords(typesStatistics);
+        }
+    }
+}
+
+static IEnumerable<string> GetRefencedTypesNames(ClrObject x)
+{
+    return x.EnumerateReferences().Select(y => y.Type?.Name ?? "");
+}
+
+class DumpType
+{
+    public string Name { get; }
+    public int Count { get; }
+    public decimal Size { get; }
+
+    public DumpType(string typeName, int count, decimal size)
+    {
+        Name = typeName;
+        Count = count;
+        Size = size;
+    }
 }
---
+++ ProccessToDump/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Starting to create objects...");
+
+var nestedObjects = Enumerable.Range(1, 1000).Select(x => new NestedObject()).ToList();
+
+Console.WriteLine("Objects created. Take dump now.");
+Console.ReadLine();
