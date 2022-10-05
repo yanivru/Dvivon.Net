df716ff3e7f95cb78b6ecd7c874cf5a6ae2dea88
yanivru <yaniv.ru@gmail.com>
2022-10-05 12:25:33 +0300

    Initial Commit

---
+++ Dvivon.Net/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Diagnostics.Runtime;
+using System.Diagnostics;
+
+Console.WriteLine("Starting memory dump essence extraction...");
+
+var processingStopwatch = Stopwatch.StartNew();
+
+var commandLineArgs = Environment.GetCommandLineArgs();
+if(commandLineArgs.Length == 1)
+{
+    Console.WriteLine("Error: Dump path not specified.");
+}
+
+using (var dataTarget = DataTarget.LoadDump(commandLineArgs[1]))
+{
+    var clrRuntime = dataTarget.ClrVersions[0].CreateRuntime();
+    IEnumerable<ClrObject> objects = clrRuntime.Heap.EnumerateObjects();
+    objects.Select(x => x.Ty)
+}
